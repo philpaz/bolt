bac78410baf24cc65cb5951ae9b7967b8f15171e
sridhar61 <sridhar.sidhu61@gmail.com>
2017-07-10 16:28:46 -0500

    Sridhar - new project setup

---
+++ Bolt/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bolt::Application.config.secret_token = '0e300c76f3d595bac713e9bc5952aed8d05b2f0cdf31b246a49ac36e2415af3c68dcb07ac93712f0f8ea2707fc2e2b65f5a772d3c4d66c4d9947e4cafd22b26b'
